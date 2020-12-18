3fb97ddbcc0f83014c0122df47bb4100f597cca8
wolverinextech <71649774+wolverinextech@users.noreply.github.com>
2020-12-19 00:47:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+  <html>
+   	<head>
+   	<title>Welcome To My Site</title>
+       <meta name="og:title" content="BXT Official"/>
+       <meta name="og:description" content="Welcome To BXT Official Site"/>
+        <link rel="stylesheet" href="style.css">
+       </head>
+       <body>
+       	<center>
+       	<h1 class="ngantuk">Welcome To Our Site</h1>
+           <p class="jancok">Maintenance Mode:)</p>
+           <p class="ajng">Dah gitu doang, ngantuk soalnya</p>
+           </center>
+       </body>
+   </html>
+
+?>
