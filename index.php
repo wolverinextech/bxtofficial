3227e021fa104a1638f61a370138f0a49b3d01be
wolverinextech <71649774+wolverinextech@users.noreply.github.com>
2020-12-19 01:44:05 +0700

    J

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
    	<title>Welcome To My Site</title>
        <meta name="og:title" content="BXT Official"/>
        <meta name="og:description" content="Welcome To BXT Official Site"/>
-        <link rel="stylesheet" href="style.css">
-       </head>
+        <link rel="stylesheet" href="style.css"/>
+        </head>
        <body>
        	<center>
        	<h1 class="ngantuk">Welcome To Our Site</h1>
@@ -12,4 +12,4 @@
            <p class="ajng">Dah gitu doang, ngantuk soalnya</p>
            </center>
        </body>
-   </html>
+</html>
